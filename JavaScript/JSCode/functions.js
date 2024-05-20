b3f15f46629f907a3a5d7388e1087059d6c76d5b
lrodriguez114 <kylereaglegale@gmail.com>
2024-05-20 12:35:09 -0400

    more js files and exercises

--- JavaScript/JSCode/functions.js
+++ JavaScript/JSCode/functions.js
@@ -3,3 +3,31 @@ function sayHello() {
 }
 
 sayHello();
+
+var sayBye = function() {
+  console.log("Bye");
+}
+
+
+function sing() {
+  console.log("AHHHHHHH");
+  console.log("TEEEEEEE");
+}
+
+function sing2() {
+  console.log("laaa dee daa");
+  console.log("laaaaa");
+}
+
+
+function sing(song) {
+  sing("laaa dee daa");
+  sing("hellooooo");
+  sing("backstreets back alright");
+}
+
+function multiply(a , b) {
+  return a * b;
+}
+
+multiply(5, 10);
