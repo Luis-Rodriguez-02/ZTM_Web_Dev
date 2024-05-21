12e958ad9ba5043f2c7cb2962a2b10dd72675be5
lrodriguez114 <kylereaglegale@gmail.com>
2024-05-21 13:37:48 -0400

    fb exercise

--- JavaScript/ExerciseObjectsAndArrays/facebook.js
+++ JavaScript/ExerciseObjectsAndArrays/facebook.js
@@ -1,16 +1,16 @@
 // Create an object and an array which we will use in our facebook exercise. 
  
 // 1. Create an object that has properties "username" and "password". Fill those values in with strings.
-var user = {
+//var user = {
   username: "Lrod2002",
   password: "shazam100"
-}
+//}
 
 // 2. Create an array which contains the object you have made above and name the array "database".
 
-var database = [user];
+//var database = [user];
 // 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
-var newsfeed = [
+//var newsfeed = [
   {
     username: "Bob",
     timeline: "",
@@ -23,4 +23,37 @@ var newsfeed = [
     username: "Andy",
     timeline: ""
   }
-]
+//]
+
+var database = [
+  {
+  username: "Luis",
+  password: "supersecret"
+  }
+];
+
+var newsfeed = [
+  {
+    username: "Bobby",
+    timeline: "So tired from all that learning"
+  },
+  {
+    username: "Sally",
+    timeline: "Javascript is sooo cool"
+  }
+];
+
+var userNamePrompt = prompt("What's your username");
+var passwordPrompt = prompt("What's your password");
+
+function signIn(username, password) {
+  if (username === database[0].username && password === database[0].password) {
+    console.log(newsfeed);
+  } else {
+    alert("Wrong username and password");
+  }
+}
+
+signIn(userNamePrompt, passwordPrompt);
+
+
