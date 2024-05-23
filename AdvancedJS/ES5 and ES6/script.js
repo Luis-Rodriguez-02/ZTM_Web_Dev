b7a408d829ccbe1cfee1f8e7072606bf10c51074
lrodriguez114 <kylereaglegale@gmail.com>
2024-05-23 13:43:56 -0400

    es5 nd 6

---
+++ AdvancedJS/ES5 and ES6/script.js
@@ -0,0 +1,21 @@
+const obj = {
+  player: "Bobby",
+  experience: 100,
+  wizardLevel: false
+}
+
+
+const player1 = obj.player;
+const experience1 = obj.experience;
+let wizardLevel1 = obj.wizardLevel;
+
+const { player, experience } = obj;
+let { wizardLevel } = obj;
+
+console.log(player);
+console.log(experience);
+
+
+
+
+
