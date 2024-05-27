4a3705cbd2c070dee502b7fc67c2627272ffa9d2
lrodriguez114 <kylereaglegale@gmail.com>
2024-05-27 12:16:37 -0400

    advanced objects

---
+++ AdvancedJS/AdvancedObjects/ExerciseAdvancedObjects/tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+//Evaluate these:
+// //#1
+// [2] === [2] 
+// {} === {} 
+
+// //#2 what is the value of property a for each object.
+// const object1 = { a: 5 };  
+// const object2 = object1; 
+// const object3 = object2; 
+// const object4 = { a: 5}; 
+// object1.a = 4;
+
+// // obj1 = 4 
+// // object2 = 4
+// // object3 = 4
+// //object4 = 5
