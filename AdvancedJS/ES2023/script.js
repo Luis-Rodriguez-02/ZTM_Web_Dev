1019edb691a1cf11670f39c7cff9727b9f6eb9ae
lrodriguez114 <kylereaglegale@gmail.com>
2024-06-05 11:38:34 -0400

    es2023

---
+++ AdvancedJS/ES2023/script.js
@@ -0,0 +1,15 @@
+// findlast
+// findLastIndex
+
+// -these keep the originals unmodified
+// toReversed
+// toSorted
+// toSpliced
+
+// with (index, modify)
+
+const ztmMonstersList = ["Mouse", "Mac", "Pye", "Denodude"];
+
+console.log(ztmMonstersList.toReversed());
+console.log(ztmMonstersList);
+
